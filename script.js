605836a97e446d664a2b035544336ca5c44bc556
rida <salman65.co@gmail.com>
2025-08-23 12:01:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,201 @@
+function locoscroll(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+function cursorEffect() {
+    var page1 = document.querySelector(".page1");
+    var cursor = document.querySelector(".cursor")
+
+  
+
+page1.addEventListener("mousemove", function (dets) {
+gsap.to(cursor, {
+
+x: dets.x,
+y: dets.y,
+duration: 0.2
+});
+});
+
+page1.addEventListener("mouseenter", function () {
+gsap.to(cursor, {
+scale: 1,
+opacity: 1,
+duration: 0.3
+});
+});
+page1.addEventListener("mouseleave", function () {
+
+gsap.to(cursor, {
+
+scale: 0,
+opacity: 0,
+
+duration: 0.3
+
+});
+
+});
+
+}
+
+
+cursorEffect();
+function videoEfect(){ 
+  let box = document.querySelectorAll('.page5 .box')
+    gsap.fromTo(
+        box,
+        { scale: 1 },
+        {
+            scale: 2,
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: box,
+                scroller: ".main", // Locomotive scroll ka scroller
+                start: "top 80%",
+                end: "top 20%",
+                scrub: true,
+                yoyo:true
+            },
+        }
+    );
+}
+
+
+
+let tl = gsap.timeline()
+
+tl.from(".loader h3", {
+
+  x:100,
+  opacity:0,
+  duration: 1,
+  stagger: 0.2,
+  delay:0.5
+
+
+})
+
+tl.to(".loader",{
+  opacity:0
+})
+
+tl.from(".page1-content h1 span",{
+  y:100,
+  opacity:0,
+  stagger:0.15,
+  duration:1,
+  delay:-0.5
+ 
+
+})
+
+tl.to(".loader",{
+  display:"none"
+})
+
+
+
+
+
+
+
+
+locoscroll();
+cursorEffect();
+videoEfect();
+
+
+gsap.from(".elem2 h1 ",{
+
+    y:120,
+    opacity:0,
+    stagger:0.25,
+    duration:1,
+    scrollTrigger:{
+        trigger:".page2",
+        scroller:".main",
+        start:"top 40%",
+        end:"top 37%",
+        scrub:2
+    }
+
+})
+
+
+const loader = document.querySelector(".loader");
+let second = document.querySelector(".second");
+
+second.addEventListener("mouseenter", () => {
+  let count = 0;
+  const interval = setInterval(() => {
+    loader.textContent = count + "%";
+    loader.style.opacity = "0.7";
+    loader.style.transform = "scale(1.05)";
+    
+    setTimeout(() => {
+      loader.style.opacity = "1";
+      loader.style.transform = "scale(1)";
+    }, 50);
+
+    count++;
+    if (count > 150) {
+      clearInterval(interval);
+    }
+  }, 5); // speed (lower = faster)
+});
+second.addEventListener("mouseleave", () => {
+  let count = 0;
+  const interval = setInterval(() => {
+    loader.textContent = count + "%";
+    loader.style.opacity = "0";
+    loader.style.transform = "scale(1.05)";
+    
+    setTimeout(() => {
+      loader.style.opacity = "0";
+      loader.style.transform = "scale(1)";
+    }, 50);
+
+    count++;
+    if (count > 150) {
+      clearInterval(interval);
+    }
+  }, 5); // speed (lower = faster)
+});
+
+
+
+
+
+
+
+
+
+
